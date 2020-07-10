e69dadcc12f36a78271b7d78dc15f26dd2bdc69d
Amit Samant <amitsamant@outlook.com>
2020-07-11 00:57:06 +0530

    Initial Commit

---
+++ Shared/Expanding_hierarchyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expanding_hierarchyApp.swift
+//  Shared
+//
+//  Created by dominator on 11/07/20.
+//
+
+import SwiftUI
+
+@main
+struct Expanding_hierarchyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
